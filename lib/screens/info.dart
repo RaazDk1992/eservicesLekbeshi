14ce9a517c8ec42d502d5e3edaaaae5b9fd057d9
RaazDk1992 <dhakal1992raaz@gmail.com>
2023-10-17 10:48:11 +0545

    dashboard ui update.

--- lib/screens/info.dart
+++ lib/screens/info.dart
@@ -54,77 +54,173 @@ class _PalikaInfoState extends State<PalikaInfo> {
           'महिला', double.parse(provider.a.data.elementAt(0).total_female!)),
       ChartData('पुरुष', double.parse(provider.a.data.elementAt(0).total_male!))
     ];
+
+    List<ChartData> elderlyData = [
+      ChartData('६५ वर्ष माथि',
+          double.parse(provider.a.data.elementAt(0).elderly_above_sxityfive!)),
+      ChartData('७० वर्ष माथि',
+          double.parse(provider.a.data.elementAt(0).elderly_above_seventy!)),
+      ChartData(
+          '७५ वर्ष माथि',
+          double.parse(
+              provider.a.data.elementAt(0).elderly_above_seventyfive!)),
+      ChartData('८० वर्ष माथि',
+          double.parse(provider.a.data.elementAt(0).elderly_above_eighty!)),
+      ChartData('८५ वर्ष माथि',
+          double.parse(provider.a.data.elementAt(0).elderly_above_eightyfive!))
+    ];
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          Container(
-            height: 100,
-            margin: EdgeInsets.all(5.0),
-            child: GridView.count(
-              crossAxisCount: 4,
-              children: <Widget>[
-                Container(
-                  height: 30,
-                  color: Color.fromRGBO(230, 7, 120, 0.5),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text('Area'),
-                      Text(provider.a.data.elementAt(0).area)
-                    ],
+      child: Container(
+        height: MediaQuery.of(context).size.height * 5 - 2,
+        child: Column(
+          children: [
+            Container(
+              height: 100,
+              margin: EdgeInsets.all(5.0),
+              child: GridView.count(
+                crossAxisCount: 4,
+                children: <Widget>[
+                  Container(
+                    height: 30,
+                    color: Color.fromRGBO(230, 7, 120, 0.5),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text('Area'),
+                        Text(provider.a.data.elementAt(0).area)
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  height: 30,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text('Population'),
-                      Text(provider.a.data.elementAt(0).total_population)
-                    ],
+                  Container(
+                    height: 30,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text('Population'),
+                        Text(provider.a.data.elementAt(0).total_population)
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  height: 30,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text('Male'),
-                      Text(provider.a.data.elementAt(0).total_male!)
-                    ],
+                  Container(
+                    height: 30,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text('Male'),
+                        Text(provider.a.data.elementAt(0).total_male!)
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  height: 30,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text('Female'),
-                      Text(provider.a.data.elementAt(0).total_female!)
-                    ],
+                  Container(
+                    height: 30,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text('Female'),
+                        Text(provider.a.data.elementAt(0).total_female!)
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Container(
-            child: SfCircularChart(
-              legend: Legend(isVisible: true),
-              series: <CircularSeries>[
-                PieSeries<ChartData, String>(
-                  dataSource: data,
-                  dataLabelSettings: DataLabelSettings(isVisible: true),
-                  xValueMapper: (ChartData d, _) => d.label,
-                  yValueMapper: (ChartData d, _) => d.value,
-                )
-              ],
+            Container(
+              child: SfCircularChart(
+                legend: Legend(isVisible: true),
+                series: <CircularSeries>[
+                  PieSeries<ChartData, String>(
+                    dataSource: data,
+                    dataLabelSettings: DataLabelSettings(isVisible: true),
+                    xValueMapper: (ChartData d, _) => d.label,
+                    yValueMapper: (ChartData d, _) => d.value,
+                  )
+                ],
+              ),
             ),
-          )
-        ],
+            Container(
+              child: Text(
+                "Elderly Population",
+                style: TextStyle(
+                    color: Colors.blueAccent, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              height: 100,
+              child: GridView.count(
+                crossAxisCount: 4,
+                children: <Widget>[
+                  Container(
+                    height: 30,
+                    color: Color.fromRGBO(230, 7, 120, 0.5),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text('Above 65'),
+                        Text(provider.a.data
+                            .elementAt(0)
+                            .elderly_above_sxityfive!)
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 30,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text('Above 70'),
+                        Text(
+                            provider.a.data.elementAt(0).elderly_above_seventy!)
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 30,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text('Above 75'),
+                        Text(provider.a.data
+                            .elementAt(0)
+                            .elderly_above_seventyfive!)
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 30,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text('Above 80'),
+                        Text(provider.a.data.elementAt(0).elderly_above_eighty!)
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: SfCircularChart(
+                legend: Legend(isVisible: true),
+                series: <CircularSeries>[
+                  PieSeries<ChartData, String>(
+                    dataSource: elderlyData,
+                    dataLabelSettings: DataLabelSettings(isVisible: true),
+                    xValueMapper: (ChartData d, _) => d.label,
+                    yValueMapper: (ChartData d, _) => d.value,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
